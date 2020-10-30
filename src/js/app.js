b650dd7f3075688b0c95d01ef9ee0aa49dd573d1
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2020-10-30 16:44:17 +0100

    add additional pages

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,56 @@ import Cart from './components/Cart.js';
 
 
 const app = {
+  initPages(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children; //wszystkie dzieci kontenera stron.
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    // let pageMatchingHash = false; //why false?
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thissApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') =='#' + pageId);
+    }
+
+  },
+
   initMenu: function() {
     const thisApp = this;
     for (let productData in thisApp.data.products){
@@ -12,7 +62,6 @@ const app = {
   },
 
   initCart: function(){
-    console.log('initCart');
     const thisApp = this;
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
@@ -38,7 +87,6 @@ const app = {
         thisApp.initMenu();
       /* execute initMenu method */
       });
-    // console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
   init: function(){
@@ -49,6 +97,7 @@ const app = {
     console.log('settings:', settings);
     console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
